8bf1534870bd2be45121f782e2d3d659e5923b96
vivshaw <hvivianshaw@gmail.com>
2018-08-23 23:02:11 -0400

    add lint-staged

--- src/renderer/puppet-renderer.provider.ts
+++ src/renderer/puppet-renderer.provider.ts
@@ -3,19 +3,18 @@ import providePuppeteer from '../inject/puppeteer.provider';
 import { PuppetRenderer } from './puppet-renderer';
 
 export async function createPuppetRenderer(port: number): Promise<PuppetRenderer> {
+  const options = {
+    port,
+    host: `http://localhost:${port}`,
+    pathParams: {
+      workingDir: process.cwd(),
+      distSubDir: 'dist',
+    },
+  };
 
-    const options = {
-        port,
-        host: `http://localhost:${port}`,
-        pathParams: {
-            workingDir: process.cwd(),
-            distSubDir: 'dist'
-        }
-    };
+  // Get injectable instances of the renderer's dependencies on Express and Puppeteer
+  const server = await provideExpress(options);
+  const puppeteer = await providePuppeteer();
 
-    // Get injectable instances of the renderer's dependencies on Express and Puppeteer
-    const server = await provideExpress(options);
-    const puppeteer = await providePuppeteer();
-
-    return new PuppetRenderer(options, server, puppeteer.browser, puppeteer.page);
+  return new PuppetRenderer(options, server, puppeteer.browser, puppeteer.page);
 }
